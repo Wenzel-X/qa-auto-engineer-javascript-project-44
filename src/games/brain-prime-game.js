537bc5496f84522bb70d29adc27bedd6291445fa
Vyacheslav Kalinichenko <wenzel.sonne@gmail.com>
2024-04-24 07:44:09 +0300

    i++

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -4,7 +4,7 @@ import getRandomInt from '../randomNumber.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  for (let i = 2; i < number; i++) {
+  for (let i = 2; i < number; i += 1) {
     if (number % i === 0) return false;
   }
   return number !== 1;
