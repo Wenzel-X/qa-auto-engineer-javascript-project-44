06c9d71855b4576fbf6366e60dc0979b98cb7ba4
Vyacheslav Kalinichenko <wenzel.sonne@gmail.com>
2024-05-15 19:55:28 +0300

    isPrime: добавляет условие для значений < 2

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -4,6 +4,8 @@ import getRandomInt from '../randomNumber.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number <= 1) return false;
+
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) return false;
   }
